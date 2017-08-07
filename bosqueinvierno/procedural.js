c5ff45129a3c65049ed58de1574819f8242ee0f0
Allison Guzmán <allison@laboratoria.cl>
2017-08-07 00:01:55 -0400

    Ejercicio 2

---
+++ bosqueinvierno/procedural.js
@@ -0,0 +1,16 @@
+function isPrime(num) {
+  //...
+}
+
+function getPrimes(start, end) {
+  for (var i = 2; i < 20; i++) {
+    var isPrime = true;
+
+    //...
+
+    if (isPrime) {
+      console.log(i);
+    }
+  }
+
+}

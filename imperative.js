e37b9c85d551f2f68b003540073448c844fad2fb
nadiatapm <nadia@laboratoria.la>
2017-08-10 13:57:35 -0400

    ejercicio 1----despues de varios errores

--- imperative.js
+++ imperative.js
@@ -1,7 +1,7 @@
 // Iteramos desde 2 (el primer número primo) hasta 19 (menor que 20)
 for (var i = 2; i < 20; i++) {
   var isPrime = true;
-  for (var j = 2; j <=(i/2); j++){
+  for (var j = 2; j <=(i / 2); j++){
      if(i % j === 0){
       isPrime = false;
     }

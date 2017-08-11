4c9320b8178d1e84e6f167b5cfb4c5762431be8c
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2017-08-10 21:25:58 -0700

    programacion orientada a objetos

---
+++ jocelynjo/oop.js
@@ -0,0 +1,39 @@
+function Note(text) {
+  this.text = text || '';
+  this.createdAt = new Date();
+  this.completed = false;
+}
+
+Note.prototype.toString = function () {
+  let str = '[' + (this.completed ? 'X' : ' ') + ']';
+  str += ' | ' + this.createdAt.toDateString();
+  str += ' | ' + this.text;
+  return str;
+};
+
+
+function Notes() {
+  this.data = [];
+}
+
+Notes.prototype.add = function (note) {
+
+  if (note instanceof Note){
+    this.data.unshift(note);
+
+ }else{
+   throw new Exception('NOOOOOOOO');
+ }
+  
+};
+
+Notes.prototype.toString = function () {
+  if(this.data.length === 0){
+    return "";
+  }else{
+    return this.data.join('\n');
+  }
+};
+
+module.exports.Note = Note
+module.exports.Notes = Notes

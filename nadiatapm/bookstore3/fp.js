6773ec09e27767e38350cd8347714114e17ed6c3
nadiatapm <nadia@laboratoria.la>
2017-08-24 20:48:32 -0300

    subo book3

---
+++ nadiatapm/bookstore3/fp.js
@@ -0,0 +1,68 @@
+const book ={
+	'title':'Libro 1',
+	'subtitle':'javascript',
+	'author':'osom author',
+	toString: function(){
+		return `${this.title} - {this.subtitle} | ${this.author}`
+	}
+}
+//console.log(JSON.stringify(book,null,2))
+//console.log(book.toString())
+//console.log(book)
+
+function createBook(){
+	return{
+		'title':'Libro 1',
+		'subtitle':'javascript',
+		'author':'osom author',
+		toString:function(){
+		return `${this.title} - {this.subtitle} | ${this.author}`
+		}
+	}
+}
+//const b = new Book()
+const b = createBook()
+console.log(JSON.stringify(b,null,2))
+//-----------
+function createBook2(title,subtitle = 'placeholder',author = 'placeholder'){
+	return{
+		title,//=> title:title
+		subtitle,
+		author,
+		toString:function(){
+		return `${this.title} - {this.subtitle} | ${this.author}`
+		}
+	}
+}
+const b2 = createBook2('Libro 2','Mastering Javascript','Yo mismo')
+//console.log(JSON.stringify(b2,null,2))
+//console.log(b2.toString())
+//----------
+
+function createBook3(title,subtitle,author){
+	const tmpTitle = title || 'default'
+	const tmpSubtitle = subtitle || 'default'
+	const tmpAuthor = author || 'default'
+
+	return{
+		title:tmpTitle,
+		subtitle:tmpSubtitle,
+		author:tmpAuthor,
+		toString:()=>{
+			return `${tmpTitle} - {tmpSubtitle} | ${tmpAuthor}`
+		}
+	}
+}
+const b3 = createBook3('Libro 3')
+console.log(b3.toString())
+//---------
+
+const b4 = Object.create({})
+console.log(b4)
+const b5 = Object.assign(b4,{//assign mezcla los dos objetos en este caso b4 con b5
+	title:'Libro 5',
+	toString:function(){ return this.title}
+})
+console.log(b5)
+console.log(Object.keys(b5))
+

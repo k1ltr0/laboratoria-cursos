86a2fe930d36a04bbc4f2b8313f6d0331e394dbe
Allison Guzmán <allison@laboratoria.cl>
2017-08-27 16:57:04 -0300

    Búsqueda de libros por título y autor

---
+++ bosqueinvierno/bookstore/imperative.js
@@ -0,0 +1,110 @@
+// si cree un directorio con mkdir y le puse mal el nombre, cambiar con mv nombremalo(espacio)nombrebueno
+// escribiendo en terminal head nombre archivo puedo ver si el archivo (books.js) está bien guardado
+
+const booksFile = require('./books.json')
+
+//console.log(JSON.stringify(books, null, 2))//me permite imprimir un objeto literal, sirve para debuggear.  El número (2) son los espacios de indentación
+//buscar libros por nombres
+//esta es la forma imperativa
+function getBooksByName(name){
+	const foundBooks = []   //agregar nombres de libros parecidos
+	
+	for ( let i = 0; i < booksFile.books.length; i++) {//voy a recorrer mi arreglo que esta guardado dentro de mi objeto
+		const book = booksFile.books[i]//voy valor a mi variable creada con la posicion [i]
+		//console.log(JSON.stringify(book, null, 2))//aca imprimo mi var creada
+		const title = book.title.toLowerCase()
+		const lowerName = name.toLowerCase()
+		//if (book.title.indexOf(name) > -1)
+		if (title.includes(lowerName)) {
+			foundBooks.push(book)//aca busco todos los libros
+		}
+	}
+		//console.log(JSON.stringify(book, null, 2))
+		//console.log('@@@@@@@@@@@@@')
+
+		return foundBooks
+	}
+//const foundBooks = getBooksByName("JavaScript")
+//console.log(foundBooks)
+
+function getBooksTitleByName(name){
+	const foundBooksTitle = []
+	
+	for ( let i = 0; i < booksFile.books.length; i++) {//voy a recorrer mi arreglo que esta guardado dentro de mi objeto
+		const book = booksFile.books[i]//voy valor a mi variable creada con la posicion [i]
+		//console.log(JSON.stringify(book, null, 2))//aca imprimo mi var creada
+		const title = book.title.toLowerCase()
+		const lowerName = name.toLowerCase()
+		//if (book.title.indexOf(name) > -1)
+		if (title.includes(lowerName)) {
+			foundBooksTitle.push(book.title)//aca busco los libros por su titulo
+		}
+	}
+		//console.log(JSON.stringify(book, null, 2))
+		//console.log('@@@@@@@@@@@@@')
+
+		return foundBooks
+	}
+
+//const foundBooksTitle = getBooksTitleByName("JavaScript")
+//console.log(foundBooksTitle)
+
+function getBooksTitleBy(key, token){
+	const foundBooks = []
+	if(key === 'name'){
+		for ( let i = 0; i < booksFile.books.length; i++) {//voy a recorrer mi arreglo que esta guardado dentro de mi objeto
+		const book = booksFile.books[i]//voy valor a mi variable creada con la posicion [i]
+		//console.log(JSON.stringify(book, null, 2))//aca imprimo mi var creada
+		const title = book[key].toLowerCase()
+		const lowerName = token.toLowerCase()
+		//if (book.title.indexOf(name) > -1)
+		if (title.includes(lowerName)) {
+			foundBooks.push(book.title)//aca busco los libros por su titulo
+			}
+		}
+	}else if(key === 'author'){
+		for ( let i = 0; i < booksFile.books.length; i++) {//voy a recorrer mi arreglo que esta guardado dentro de mi objeto
+		const book = booksFile.books[i]//voy valor a mi variable creada con la posicion [i]
+		//console.log(JSON.stringify(book, null, 2))//aca imprimo mi var creada
+		const title = book[key].toLowerCase()
+		const lowerName = token.toLowerCase()
+		//if (book.title.indexOf(name) > -1)
+		if (title.includes(lowerName)) {
+			foundBooks.push(book.title)//aca busco los libros por su titulo
+				}
+		}
+	return foundBooks
+	}
+}
+//const foundBooks = getBooksTitleBy('title', 'JavaScript')
+//console.log(foundBooks)
+//const foundBooks_2 = getBooksTitleBy('author', 'JavaScript')
+//console.log(foundBooks_2)
+
+function getBooksTitleByV2(key, token){
+	const currentKey = selectKey(key)
+	const lowerToken = token.toLowerCase()
+	return booksFile.books.filter((book)=>{
+		const title = book[currentKey].toLowerCase()
+		return title.includes(lowerToken)
+	}).map((book)=> book.title)
+}
+
+function selectKey(key){
+	let currentKey = ''
+	if(key === 'title'){
+		currentKey = key
+	}else if (key === 'author'){
+		currentKey = key
+	}
+	return currentKey
+}
+const foundBooks = getBooksTitleByV2('title', 'Javascript')
+console.log(foundBooks)
+console.log('#######################')
+const foundBooksDos = getBooksTitleByV2('author', 'Haverbeke')
+console.log(foundBooksDos)
+
+
+//Lodash https://lodash.com/ (buscar cómo usar y para qué puede servir)
+//Modulo fs (file system, modificar el archivo) /buscar
